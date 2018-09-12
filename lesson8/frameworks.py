9f8a7d08de00ca2b10ed0edbe5fb2f07b82ae98f
root <root@localhost.localdomain>
2018-09-12 12:18:52 +0300

    class on python frameworks-Flask

---
+++ lesson8/frameworks.py
@@ -0,0 +1,11 @@
+#  Framework : is a set of structures, design that uses different programming languages
+
+#  a framework is not a programming language
+
+#  advantages:  easy to program, better security, advanced features, speed up processing
+
+#  python frameworks:  Bottle, Pyramid, Flask, Django, web2y
+
+#  Flask uses HTML/CSS/Python/Jinja/SQL
+
+#  flask can be used to create, websites, web-applications: management systems
